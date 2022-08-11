b573a0112f709d70d86a30e7edf6403bac962574
Terence Tuhinanshu <terence@tuhinanshu.com>
2022-08-11 13:30:22 -0400

    Rename: Main -> Draw
    Draw is a better descriptor of what that page is about.

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -2,14 +2,14 @@ import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import Welcome from './pages/Welcome';
-import Main from './pages/Main';
+import Draw from './pages/Draw';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/welcome' element={<Welcome />} />
-                <Route path='/main' element={<Main />} />
+                <Route path='/draw' element={<Draw />} />
                 <Route path='/' element={<Navigate to='/welcome' replace />} />
             </Routes>
         </BrowserRouter>
