ba21d58943df4ef34e8d7913aaae734ba8224f8b
Terence Tuhinanshu <terence@tuhinanshu.com>
2022-08-27 14:58:11 -0400

    Add Topographic basemap for Land & water layer
    Since we can't add individual vector tiles, we switch the basemap
    wholesale.

--- src/app/src/components/Layers/Basemaps.js
+++ src/app/src/components/Layers/Basemaps.js
@@ -3,6 +3,7 @@ import L from 'leaflet';
 import { useSetMaxZoomLevel, useMapLayer } from '../../hooks';
 import {
     DEFAULT_BASEMAP_URL,
+    TOPOGRAPHIC_BASEMAP_URL,
     ESRI_ATTRIBUTION,
     SATELLITE_BASEMAP_URL,
 } from '../../constants';
@@ -11,6 +12,16 @@ export function DefaultBasemap() {
     return (
         <Basemap
             url={DEFAULT_BASEMAP_URL}
+            attribution={ESRI_ATTRIBUTION}
+            maxZoomLevel={16}
+        />
+    );
+}
+
+export function TopographicBasemap() {
+    return (
+        <Basemap
+            url={TOPOGRAPHIC_BASEMAP_URL}
             attribution={ESRI_ATTRIBUTION}
             maxZoomLevel={16}
         />
