48258245cbc99209717ac5ea58e309c3ca7a2617
Terence Tuhinanshu <terence@tuhinanshu.com>
2022-08-29 17:52:55 -0400

    Show parcel centroids at lower zoom levels
    This may be useful to guide the user even when the zoom is
    too far up.

--- src/app/src/components/Layers/ParcelLayer.js
+++ src/app/src/components/Layers/ParcelLayer.js
@@ -6,29 +6,48 @@ import { DynamicMapLayer } from 'esri-leaflet';
 import { useMapLayer } from '../../hooks';
 import { PARCELS_LAYER_URL, DATA_LAYERS } from '../../constants';
 
-const PARCELS_LAYER_STYLE = {
-    id: 1,
-    source: {
-        type: 'mapLayer',
-        mapLayerId: 1,
-    },
-    drawingInfo: {
-        renderer: {
-            type: 'simple',
-            symbol: {
-                type: 'esriSFS',
-                style: 'esriSFSSolid',
-                color: [0, 0, 0, 0], // No fill
-                outline: {
-                    type: 'esriSLS',
-                    style: 'esriSLSSolid',
+const PARCELS_LAYER_STYLE = [
+    {
+        id: 0,
+        source: {
+            type: 'mapLayer',
+            mapLayerId: 0,
+        },
+        drawingInfo: {
+            renderer: {
+                type: 'simple',
+                symbol: {
+                    type: 'esriSMS',
+                    style: 'esriSMSDiamond',
                     color: [183, 121, 31, 255], // var(--chakra-colors-yellow-600)
-                    width: 1.0,
                 },
             },
         },
     },
-};
+    {
+        id: 1,
+        source: {
+            type: 'mapLayer',
+            mapLayerId: 1,
+        },
+        drawingInfo: {
+            renderer: {
+                type: 'simple',
+                symbol: {
+                    type: 'esriSFS',
+                    style: 'esriSFSSolid',
+                    color: [0, 0, 0, 0], // No fill
+                    outline: {
+                        type: 'esriSLS',
+                        style: 'esriSLSSolid',
+                        color: [183, 121, 31, 255], // var(--chakra-colors-yellow-600)
+                        width: 1.0,
+                    },
+                },
+            },
+        },
+    },
+];
 
 export default function ParcelLayer() {
     const layers = useSelector(state => state.map.layers);
@@ -44,7 +63,7 @@ function RenderEsriLayer() {
             f: 'image',
             layers: [0, 1],
             minZoom: DATA_LAYERS['PARCELS'].minZoom, // Only enable on high zooms for performance
-            dynamicLayers: JSON.stringify([PARCELS_LAYER_STYLE]),
+            dynamicLayers: JSON.stringify(PARCELS_LAYER_STYLE),
         })
     );
 }
