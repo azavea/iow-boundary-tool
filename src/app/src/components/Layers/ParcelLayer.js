028b998facb025e964a515a64c70b82674fb1cbe
Terence Tuhinanshu <terence@tuhinanshu.com>
2022-08-29 17:40:55 -0400

    Add Yellow 600 styling to image requests
    The default styling is hot pink. Our Yellow 600 customization
    adds a slight delay to requests, but this may be tolerable.

--- src/app/src/components/Layers/ParcelLayer.js
+++ src/app/src/components/Layers/ParcelLayer.js
@@ -6,6 +6,30 @@ import { DynamicMapLayer } from 'esri-leaflet';
 import { useMapLayer } from '../../hooks';
 import { PARCELS_LAYER_URL, DATA_LAYERS } from '../../constants';
 
+const PARCELS_LAYER_STYLE = {
+    id: 1,
+    source: {
+        type: 'mapLayer',
+        mapLayerId: 1,
+    },
+    drawingInfo: {
+        renderer: {
+            type: 'simple',
+            symbol: {
+                type: 'esriSFS',
+                style: 'esriSFSSolid',
+                color: [0, 0, 0, 0], // No fill
+                outline: {
+                    type: 'esriSLS',
+                    style: 'esriSLSSolid',
+                    color: [183, 121, 31, 255], // var(--chakra-colors-yellow-600)
+                    width: 1.0,
+                },
+            },
+        },
+    },
+};
+
 export default function ParcelLayer() {
     const layers = useSelector(state => state.map.layers);
     const showLayer = useMemo(() => layers.includes('PARCELS'), [layers]);
@@ -20,6 +44,7 @@ function RenderEsriLayer() {
             f: 'image',
             layers: [0, 1],
             minZoom: DATA_LAYERS['PARCELS'].minZoom, // Only enable on high zooms for performance
+            dynamicLayers: JSON.stringify([PARCELS_LAYER_STYLE]),
         })
     );
 }
