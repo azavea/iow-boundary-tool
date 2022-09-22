62f293c0709545169a755727a26858d378644508
Jacob Walls <jwalls@azavea.com>
2022-09-22 11:00:12 -0400

    Add `dj_rest_auth` endpoints for login, logout, etc

--- src/django/api/urls.py
+++ src/django/api/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
-    path('example/', views.example),
+    path("auth/login/", views.Login.as_view()),
+    path("auth/logout/", views.Logout.as_view()),
+    path("auth/", include("dj_rest_auth.urls")),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
