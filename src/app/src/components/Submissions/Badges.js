9a764aa15f114c4bbfbe6047ec57c1b539191631
Terence Tuhinanshu <terence@tuhinanshu.com>
2022-10-12 22:15:09 -0400

    Add parameterizable Badge
    This can be given a status and will show up appropriately.

--- src/app/src/components/Submissions/Badges.js
+++ src/app/src/components/Submissions/Badges.js
@@ -1,5 +1,7 @@
 import { Badge } from '@chakra-ui/react';
 
+import { BOUNDARY_STATUS } from '../../constants';
+
 export function SubmittedBadge({ variant = 'solidFixedHeight' }) {
     return (
         <Badge bg='teal.400' variant={variant} colorScheme='green'>
@@ -12,6 +14,22 @@ export function DraftBadge({ variant = 'solidFixedHeight' }) {
     return (
         <Badge variant={variant} colorScheme='yellow'>
             DRAFT
+        </Badge>
+    );
+}
+
+const BADGE_COLORS = {
+    [BOUNDARY_STATUS.DRAFT]: 'gray',
+    [BOUNDARY_STATUS.SUBMITTED]: 'teal',
+    [BOUNDARY_STATUS.IN_REVIEW]: 'orange',
+    [BOUNDARY_STATUS.NEEDS_REVISIONS]: 'orange',
+    [BOUNDARY_STATUS.APPROVED]: 'green',
+};
+
+export function StatusBadge({ status }) {
+    return (
+        <Badge variant='solid' colorScheme={BADGE_COLORS[status]}>
+            {status.toUpperCase()}
         </Badge>
     );
 }
