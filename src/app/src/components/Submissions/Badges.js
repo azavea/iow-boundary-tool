0fb5c48c29e1fff939aade4881826dbc9d2ea510
Terence Tuhinanshu <terence@tuhinanshu.com>
2022-11-11 16:46:10 -0500

    Update badge color for In Review
    Closes #187

--- src/app/src/components/Submissions/Badges.js
+++ src/app/src/components/Submissions/Badges.js
@@ -5,7 +5,7 @@ import { BOUNDARY_STATUS } from '../../constants';
 const BADGE_COLORS = {
     [BOUNDARY_STATUS.DRAFT]: 'gray',
     [BOUNDARY_STATUS.SUBMITTED]: 'teal',
-    [BOUNDARY_STATUS.IN_REVIEW]: 'orange',
+    [BOUNDARY_STATUS.IN_REVIEW]: 'yellow',
     [BOUNDARY_STATUS.NEEDS_REVISIONS]: 'orange',
     [BOUNDARY_STATUS.APPROVED]: 'green',
 };
