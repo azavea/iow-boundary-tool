b47f66a51008d29ce5e816af0e963c1cb6f4c9dd
Terence Tuhinanshu <terence@tuhinanshu.com>
2022-11-21 18:44:31 -0500

    Initialize user map on utility location
    Now that we have live utilities, instead of always initializing
    the map on the same North Carolina spot, we initialize it where
    the selected utility's centroid is, falling back to the old
    default if necessary.

--- src/app/src/components/Map.js
+++ src/app/src/components/Map.js
@@ -1,4 +1,5 @@
 import { MapContainer } from 'react-leaflet';
+import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 
 import { MAP_CENTER, MAP_INITIAL_ZOOM, NAVBAR_HEIGHT } from '../constants';
@@ -6,6 +7,10 @@ import MapPanes from './MapPanes';
 
 export default function Map({ children }) {
     const location = useLocation();
+    const utility = useSelector(state => state.auth.utility);
+
+    const coordinates = utility?.location?.coordinates;
+    const center = coordinates ? [coordinates[1], coordinates[0]] : MAP_CENTER;
 
     // Anywhere but welcome activity, deduct height of nav bar
     const heightExpression = location.pathname.startsWith('/welcome')
@@ -14,7 +19,7 @@ export default function Map({ children }) {
 
     return (
         <MapContainer
-            center={MAP_CENTER}
+            center={center}
             zoom={MAP_INITIAL_ZOOM}
             zoomControl={false}
             style={{ height: heightExpression }}
