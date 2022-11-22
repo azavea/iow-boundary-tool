7069764fc46bbe8b49fe3afc8a275edc64e5a26d
Terence Tuhinanshu <terence@tuhinanshu.com>
2022-11-22 17:24:12 -0500

    Remove unused settings button
    This page was never made, and that information can be edited
    in the Django Admin.

--- src/app/src/components/NavBar.js
+++ src/app/src/components/NavBar.js
@@ -4,12 +4,11 @@ import {
     SimpleGrid,
     Heading,
     Icon,
-    IconButton,
     Spacer,
 } from '@chakra-ui/react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { ArrowLeftIcon, CogIcon, LogoutIcon } from '@heroicons/react/outline';
+import { ArrowLeftIcon, LogoutIcon } from '@heroicons/react/outline';
 import apiClient from '../api/client';
 import { API_URLS, NAVBAR_HEIGHT } from '../constants';
 import { logout } from '../store/authSlice';
@@ -43,25 +42,10 @@ export default function NavBar({ variant }) {
 
             <Flex align='center' gap={4}>
                 <Spacer />
-                <SettingsButton variant={variant} />
                 <ExitButton variant={variant} />
             </Flex>
         </SimpleGrid>
     );
-}
-
-function SettingsButton({ variant }) {
-    const navigate = useNavigate();
-
-    return variant === NAVBAR_VARIANTS.SUBMISSION ? (
-        <IconButton
-            icon={<Icon as={CogIcon} />}
-            aria-label='Settings'
-            onClick={() => {
-                navigate('/settings');
-            }}
-        />
-    ) : null;
 }
 
 function ExitButton({ variant }) {
