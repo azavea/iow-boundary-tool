7012265dfba645103f4946c4f7d59da719041f7c
Terence Tuhinanshu <terence@tuhinanshu.com>
2023-06-12 10:47:16 -0400

    WIP2

--- src/django/api/management/commands/create_contributors_from_s3.py
+++ src/django/api/management/commands/create_contributors_from_s3.py
@@ -5,7 +5,7 @@ from botocore.exceptions import ClientError
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
-from api.models import Roles, User
+from api.models import Roles, User, Utility
 
 
 class Command(BaseCommand):
@@ -18,13 +18,13 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        bucket_name = options['bucket_name']
-        csv_file_key = options['csv_file_key']
+        bucket_name = options["bucket_name"]
+        csv_file_key = options["csv_file_key"]
 
         try:
-            s3 = boto3.client('s3')
+            s3 = boto3.client("s3")
             response = s3.get_object(Bucket=bucket_name, Key=csv_file_key)
-            content = response['Body'].read().decode('utf-8')
+            content = response["Body"].read().decode("utf-8")
 
             reader = csv.DictReader(content.splitlines())
             with transaction.atomic():
@@ -39,10 +39,37 @@ class Command(BaseCommand):
                             job_title=row["job_title"],
                         )
 
-                        self.stdout.write(f"Successfully created user: {user.email}")
+                        # Associate with utilities matching given ; separated PWSIDs.
+                        # We don't use .filter(pwsid__in) here because we want to fail
+                        # if any given PWSID is not found in the system to call out
+                        # mistakes in input CSV.
+                        pwsids = row["pwsids"].split(";")
+                        utilities = []
+                        for pwsid in pwsids:
+                            try:
+                                utilities.append(Utility.objects.get(pwsid=pwsid))
+                            except Utility.DoesNotExist:
+                                self.stderr.write(
+                                    f"Invalid PWSID '{pwsid}' "
+                                    f"for contributor {user.email}"
+                                )
+                                raise
+
+                        user.utilities.set(utilities)
+
+                        utilities_str = ", ".join(
+                            [str(ut) for ut in user.utilities.all()]
+                        )
+
+                        self.stdout.write(
+                            f"Successfully created contributor {user.email}, "
+                            f"associated with {utilities_str}"
+                        )
 
                 except Exception as e:
-                    self.stderr.write(f"Error occurred during user creation: {str(e)}")
+                    self.stderr.write(
+                        f"Error occurred during contributor creation: {str(e)}"
+                    )
                     transaction.set_rollback(True)
         except ClientError as e:
-            self.stderr.write(f'Error occurred while accessing the CSV file: {str(e)}')
+            self.stderr.write(f"Error occurred while accessing the CSV file: {str(e)}")
